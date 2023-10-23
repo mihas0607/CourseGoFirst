55e464663f803cd658d0a17c0b55527a39b22f5a
Mikhail H <mikhail.hlus@gmail.com>
2023-10-23 15:05:00 +0300

    aded Lec2

---
+++ Lec2/main.go
@@ -0,0 +1,48 @@
+package main
+
+import (
+	"fmt"
+	//"math"
+)
+
+func main() {
+	//fmt.Print("Это моя вторая программа! Я рад, что учусь здесь!!")
+	//fmt.Println("Hello world")
+	//fmt.Println("Second line")
+	//fmt.Printf("My name is %s\nMy edge is %d\n", "Mihas", 37)
+
+// var age bool
+
+// fmt.Println("My edge:", age)
+// fmt.Printf("%T\n", age)
+
+// var firstVar, secondVar int = 10, 20
+// fmt.Printf("First is %d Second is %d\n", firstVar, secondVar)
+
+// var (
+// 	personName string = "Bob"
+// 	personAge int = 29
+// 	personUid int
+// )
+
+// fmt.Printf("Name: %s\nAge: %d\nID: %d", personName, personAge, personUid)
+
+// a, b := 30, "Vova"
+// fmt.Println(a,b)
+// a, c := 300, 400
+// fmt.Println(a,c)
+
+// width, lenght := 20.5, 30.2
+// fmt.Printf("Min demensional of rectangle is : %.3f\n", math.Min(width,lenght))
+
+
+word1 := "имя"
+word2 := "твое"
+word3 := "мне"
+word4 := "знакомо"
+
+fmt.Println(word4, word3, word2, word1)
+fmt.Println(word3, word1, word4, word2)
+fmt.Println(word2, word4, word1, word3)
+
+}
